045aa4b750553927641f0a5cb2ec7eb4609bd93a
Tadiwa Leon Muguta <leonmuguta7@gmail.com>
2022-01-24 11:21:01 +0200

    Final version of the To-Do List

--- functions.js
+++ functions.js
@@ -1,19 +1,8 @@
-// Create a "close" button and append it to each list item
-// var myNodelist = document.getElementsByTagName("li");
-// var i;
-// for (i = 0; i < myNodelist.length; i++) {
-//   var span = document.createElement("SPAN");
-//   var txt = document.createTextNode("\u00D7");
-//   span.className = "close";
-//   span.appendChild(txt);
-//   myNodelist[i].appendChild(span);
-// }
-
 //Adding item to the To-Do List
 function newElement() {
-    var li = document.createElement("li");
-    var inputValue = document.getElementById("enteredTask").value;
-    var t = document.createTextNode(inputValue);
+    let li = document.createElement("li");
+    let inputValue = document.getElementById("enteredTask").value;
+    let t = document.createTextNode(inputValue);
     li.appendChild(t);
     if (inputValue === '') {
       alert("You must write something!");
@@ -22,16 +11,46 @@ function newElement() {
     }
     document.getElementById("enteredTask").value = "";
   
-    // var span = document.createElement("SPAN");
-    // var txt = document.createTextNode("\u00D7");
-    // span.className = "close";
-    // span.appendChild(txt);
-    // li.appendChild(span);
+    let span = document.createElement("span");
+    let txt = document.createTextNode("\u00D7");
+    span.className = "close";
+    span.appendChild(txt);
+    li.appendChild(span);
   
-    // for (i = 0; i < close.length; i++) {
-    //   close[i].onclick = function() {
-    //     var div = this.parentElement;
-    //     div.style.display = "none";
-    //   }
-    // }
+    for (i = 0; i < close.length; i++) {
+      close[i].onclick = function() {
+        let div = this.parentElement;
+        div.style.display = "none";
+      }
+    }
 }
+
+// Create a "close" button and append it to each list item
+// let myNodelist = document.getElementsByTagName("li");
+
+// for (let i = 0; i < myNodelist.length; i++) {
+//   let span = document.createElement("span");
+//   let txt = document.createTextNode("\u00D7");
+//   span.className = "close";
+//   span.appendChild(txt);
+//   myNodelist[i].appendChild(span);
+// }
+
+// Click on a close button to hide the current list item
+let close = document.getElementsByClassName("close");
+
+for (let i = 0; i < close.length; i++) {
+  close[i].onclick = function() {
+    let div = this.parentElement;
+    div.style.display = "none";
+  }
+}
+
+// Add a "checked" symbol when clicking on a list item
+let list = document.querySelector('ul');
+list.addEventListener('click', function(ev) {
+  console.log(ev.target.tagName)
+  if (ev.target.tagName === 'LI') {
+    ev.target.classList.toggle('checked');
+  }
+});
